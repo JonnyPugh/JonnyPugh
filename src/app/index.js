c33b935275bf2a3c2fb493b7737e5825864b7251
Jonathan Pugh <jpugh@umich.edu>
2019-09-01 16:40:54 -0400

    Created module for app container (#2)

---
+++ src/app/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Container } from '@material-ui/core';
+import Navbar from '../navigation';
+import About from '../about';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Navbar />
+    <Container maxWidth="md">
+      <About />
+    </Container>
+  </ThemeProvider>
+);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
-import App from './App';
-import theme from './theme';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root'),
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
