c66296d74766c01a2f36e49e0094ff448091a2b2
Jon Pugh <jpugh@umich.edu>
2019-09-02 17:27:21 -0400

    Changed color palette to dark

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createMuiTheme } from '@material-ui/core/styles'
-import { red } from '@material-ui/core/colors'
 import { ThemeProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { Container } from '@material-ui/core'
@@ -9,18 +8,7 @@ import About from '../about'
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
+    type: 'dark',
   },
 })
 
